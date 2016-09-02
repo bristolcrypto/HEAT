6b71609eae6ed9727eb30a1ca6671bba49d2df7f
svivekv <svivekv@gmail.com>
2016-09-02 19:17:48 +0100

    Updating init.sh and REAME.txt files.

--- init.sh
+++ init.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
-cd helib/
-make
-cd api-bridge/
-make
-cd ../..
-make
+if [[ -d "FV-NFLlib" && ! -L "FV-NFLlib" ]] ; then
+	echo -e "\n*****Compiling the NFLlib library and the bridge API files.*****\n" 
+	cd FV-NFLlib-api
+	make
+	cd ..
+else
+	echo -e "\nFV-NFLlib library does not exist.\n If you gave used Git to download this API library, then execute "git submodule init" first and then "git submodule update".\n"
+fi
+
+if [[ -d "HElib" && ! -L "HElib" ]] ; then
+	echo -e "\n*****Compiling the HElib library and the bridge API files.*****\n"
+	cd HElib/src
+	make
+	cd ../../HElib-api
+	make
+	cd ../
+else
+	echo -e "\nHElib library does not exist.\n If you gave used Git to download this API library, then execute "git submodule init" first and then "git submodule update".\n"
+fi
 
